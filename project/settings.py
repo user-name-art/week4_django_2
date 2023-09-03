c6ec23b068d8831298db82000fdde31cf0a08e66
Art Gilyazov <art.gilyazov@mail.ru>
2023-09-03 14:15:21 +0300

    move allowed_hosts to env

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ DEBUG = env.bool('DEBUG_MODE')
 
 ROOT_URLCONF = 'project.urls'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
